1a60e3ee792034ee664701e082e8bfa1451f1f7c
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-09 14:07:55 +0530

    Update psscript.groovy

--- vars/psscript.groovy
+++ vars/psscript.groovy
@@ -1,5 +1,5 @@
 def call() {
     def scriptcontents = libraryResource "Powershell/Scripts/hello.ps1"    
     writeFile file: hello.ps1
-    powershell -File hello.ps1
+    powershell(script: 'Powershell/Scripts/hello.ps1')
 }
