42d5e93d3ec23b3304537230237801d2c034f1e0
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-09 21:08:25 +0530

    Update pass_build.groovy

--- vars/pass_build.groovy
+++ vars/pass_build.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    def scriptcontents = libraryResource "resources/Powershell/Scripts/oddOrEven.sh"    
+    def scriptcontents = libraryResource "Powershell/Scripts/oddOrEven.sh"    
     writeFile file: "${config.name}", text: scriptcontents 
     sh "chmod a+x ./${config.name}"
     sh "./${config.name} ${config.number}"
