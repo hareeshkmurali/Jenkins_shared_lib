312bb60f31e5d49bd53e1576861a53e74a06a334
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-15 17:18:46 +0530

    Update myscript.groovy

--- vars/myscript.groovy
+++ vars/myscript.groovy
@@ -4,19 +4,24 @@ def renameFile(Map scriptParams) {
     def oldName = scriptParams.OldName
     def newName = scriptParams.NewName
 
-    // PowerShell script content
-    def powerShellScript = """
-        \$oldName = "${'$'}{oldName}"
-        \$newName = "${'$'}{newName}"
-        
-        Rename-Item -Path \$oldName -NewName \$newName
-    """
+    if (oldName && newName) {
+        // PowerShell script content
+        def powerShellScript = """
+            \$oldName = "${'$'}{oldName}"
+            \$newName = "${'$'}{newName}"
+            
+            Rename-Item -Path \$oldName -NewName \$newName
+        """
 
-    // Execute the PowerShell script
-    def result = powershell(returnStdout: true, script: powerShellScript)
+        // Execute the PowerShell script
+        def result = powershell(returnStdout: true, script: powerShellScript)
 
-    // Handle the script execution result if needed
-    // ...
+        // Handle the script execution result if needed
+        // ...
+    } else {
+        error("Invalid parameters. Please provide values for OldName and NewName.")
+    }
 }
 
 return this
+
