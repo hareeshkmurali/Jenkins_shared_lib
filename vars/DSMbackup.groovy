2933842e155d80357a0634a76781f0b99bd338a4
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-15 11:38:27 +0530

    Create DSMbackup.groovy

---
+++ vars/DSMbackup.groovy
@@ -0,0 +1,12 @@
+def call(Map stageParams) {
+
+    checkout([
+        BuildNumber: [[name:  stageParams.BuildNumber ]],
+        UserName: [[ url: stageParams.UserName ]]
+        Password: [[ url: stageParams.Password ]]
+        ComputerName: [[ url: stageParams.ComputerName ]]
+        ServiceName: [[ url: stageParams.ServiceName ]]
+        SourcePath: [[ url: stageParams.SourcePath ]]
+        DirectoryPath: [[ url: stageParams.DirectoryPath ]]
+    ])
+  }
