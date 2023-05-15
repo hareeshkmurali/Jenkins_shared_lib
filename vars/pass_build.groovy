846144a9e59bedb3516520f77a9a386e724901da
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-15 09:56:19 +0530

    Delete pass_build.groovy

--- vars/pass_build.groovy
+++
@@ -1,6 +0,0 @@
-def call(Map config = [:]) {
-    def scriptcontents = libraryResource "Powershell/Scripts/oddOrEven.sh"    
-    writeFile file: "${config.name}", text: scriptcontents 
-    sh "chmod a+x ./${config.name}"
-    sh "./${config.name} ${config.number}"
-}
