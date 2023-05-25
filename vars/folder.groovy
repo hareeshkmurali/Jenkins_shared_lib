e07ae607b15e702de8c17ac1e7e82c6bbd6583ef
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-25 16:23:42 +0530

    Update folder.groovy

--- vars/folder.groovy
+++ vars/folder.groovy
@@ -1,5 +1,5 @@
 def call(String oldname, String newname) { 
-    def scriptPath = libraryResource('Powershell/Scripts/folder.ps1')
+    def scriptPath = libraryResource "Powershell/Scripts/folder.ps1"
 
     bat "powershell.exe -File ${scriptPath} -oldname '${oldname}' -newname '${newname}'"
 }
