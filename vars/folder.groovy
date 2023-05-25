de52f00384919bb9c282040c20b937888b611afb
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-25 18:33:03 +0530

    Update folder.groovy

--- vars/folder.groovy
+++ vars/folder.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) { 
     def scriptcontents = libraryResource "Powershell/Scripts/folder.ps1"
-    writeFile file: "${config.name}", text: scriptcontents 
+    writeFile file: "${config.name}", text: scriptcontents
+    bat "powershell -ExecutionPolicy ByPass -File C:\\Users\\devopsdemo123\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\sharelibpipeline3\\sample.ps1"
     powershell(script: "${config.name}", args: "${config.oldname} ${config.newname}")
 }
