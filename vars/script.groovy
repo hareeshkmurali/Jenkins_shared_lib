091adb734b3925e9bf156f9c75148dfd320af972
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 11:09:17 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -2,7 +2,7 @@ def call(String oldName, String newName) {
     def scriptContent = libraryResource('Powershell/Scripts/script.ps1')
     writeFile file: "new.ps1", text: scriptContent
     // Construct the PowerShell command
-    def powershellCommand = "powershell(script: "new.ps1")"
+    def powershellCommand = "powershell -Command \"& { ${scriptContent} }\""
     
     // Pass the parameters as arguments to the PowerShell command
     def arguments = "-oldName '${oldName}' -newName '${newName}'"
