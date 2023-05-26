fcbe801e24b8bd356c9bf23bb154d959c74a76c8
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 17:19:33 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,6 +1,6 @@
 def call(String oldName, String newName) {
     writeFile file: 'tempFile.ps1', text: "${libraryResource 'Powershell/Scripts/script.ps1'}"
-    powershell './tempFile.ps1 -oldName $oldName -newName $newName'
+    powershell './tempFile.ps1 -oldName ${oldName} -newName ${newName}'
 }
 
 
