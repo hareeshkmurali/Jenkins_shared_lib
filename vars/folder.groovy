91c90f93abd2e49132e8ef537c04849f82f35bed
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 09:50:03 +0530

    Update folder.groovy

--- vars/folder.groovy
+++ vars/folder.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
     def scriptcontents = libraryResource "Powershell/Scripts/folder.ps1"
     writeFile file: "${config.name}", text: scriptcontents
-    powershell(script: "${config.name}")
+    powershell(script: "${config.name}" oldname: "${config.oldname}" newname: "${config.newname}")
 }
