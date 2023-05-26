afeedcd235d6cba9c38ad2b7bace4d1c040f8a02
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 17:27:01 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,6 +1,6 @@
 def call(String oldName, String newName) {
     writeFile file: 'tempFile.ps1', text: "${libraryResource 'Powershell/Scripts/script.ps1'}"
-    bat "powershell -ExecutionPolicy ByPass -File "C:\\Users\\devopsdemo123\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\sharedlibpipeline4\\tempFile.ps1""
+    bat "powershell -ExecutionPolicy ByPass -File "C:Users\\devopsdemo123\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\sharedlibpipeline4\\tempFile.ps1""
     powershell './tempFile.ps1 -oldName ${oldName} -newName ${newName}'
 }
 
