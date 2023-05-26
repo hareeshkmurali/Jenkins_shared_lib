42e1d6d0ab403bd3dbe8edc37949818ef02e0a8c
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 12:24:37 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,10 +1,8 @@
 def call(String argument1, String argument2) {
-    def oldName = scriptParams.argument1
-    def newName = scriptParams.argument2
     def scriptContent = libraryResource('Powershell/Scripts/script.ps1')
     writeFile file: "new.ps1", text: scriptContent
     // Construct the PowerShell command with arguments
-    def powershellCommand = "powershell -Command \"& { ${scriptContent} }\" -ArgumentList '${oldName}', '${newName}'"
+    def powershellCommand = "powershell -Command \"& { ${scriptContent} }\" -ArgumentList @{ 'argument1' = '${argument1}'; 'argument2' = '${argument2}' }"
     
     // Execute the PowerShell command
     bat(powershellCommand)
