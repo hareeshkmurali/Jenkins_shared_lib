d353d7bb7d84da13533cb281b7a0a7952b4fe01c
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 12:15:12 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,4 +1,4 @@
-def call(Map config = [:]) {
+def call(String argument1, String argument2) {
     def oldName = scriptParams.argument1
     def newName = scriptParams.argument2
     def scriptContent = libraryResource('Powershell/Scripts/script.ps1')
