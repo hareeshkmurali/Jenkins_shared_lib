51e87cc3ae3140b8a82c02c3ac3da24285c88355
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-26 10:48:35 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,13 +1,11 @@
-def call(Map<String, String> parameters) {
-    def scriptContent = libraryResource('Powershell/Scripts/script.ps1')
+def call(String oldName, String newName) {
+    def scriptContent = libraryResource('script.ps1')
     
     // Construct the PowerShell command
     def powershellCommand = "powershell -Command \"& { ${scriptContent} }\""
     
-    // Construct the arguments for the PowerShell command
-    def arguments = parameters.collect { oldname, newname ->
-        "-${oldname} '${newname}'"
-    }.join(' ')
+    // Pass the parameters as arguments to the PowerShell command
+    def arguments = "-oldName '${oldName}' -newName '${newName}'"
     
     // Execute the PowerShell command
     bat("${powershellCommand} ${arguments}")
